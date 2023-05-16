8901d3edc412b300e11bc45f7f76e5cd76d6c170
EnderPoint07 <anshhd64@hotmail.com>
2023-05-16 22:16:51 +0530

    Ok so i decided what cos(phi) and sin(phi) were literally just cos and sin of B. I also attempted to implement z axis and step by step rotating and better explanations in 3d-circle.py. Somebody please free me from this torcher HELP ME

--- 3d-circle.py
+++ 3d-circle.py
@@ -23,7 +23,7 @@ def reset_output():
 
 
 def compute_circle(Rx, Ry, Rz):
-    # formula for circle -> x^2 + y^2 = r^2
+    # Formula for circle -> x^2 + y^2 = r^2
 
     # equation for each point on the circle
     # x = cos(theta) * radius
